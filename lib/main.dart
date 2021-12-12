638e8a15299530d49128dcd3f6cb9fe22509cb93
Thora Ingvars <t.ingvarsdottir@gmail.com>
2021-12-12 11:28:31 +0000

    update 12.12

--- lib/main.dart
+++ lib/main.dart
@@ -3,50 +3,70 @@ import 'library.dart';
 import 'dart:math';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MaterialApp(
+    title: 'Flutter',
+    home: FirstPage(),
+  ),
+  );
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class FirstPage extends StatelessWidget {
+  int turnCounter = 0;
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "First page",
+              textAlign: TextAlign.center,
+            ),
+            Container(
+              margin: const EdgeInsets.only(top: 50.0),
+              width: MediaQuery.of(context).size.width,
+              child: Center(
+                child: Container(
+                  width: 150.0,
+                  height: 50.0,
+                  color: Colors.amber[600],
+                  child: TextButton(
+                    style: TextButton.styleFrom(
+                      padding: const EdgeInsets.all(16.0),
+                      primary: Colors.red,
+                      textStyle: const TextStyle(fontSize: 15),
+                    ),
+                    onPressed: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => QuizPage(turnCounter),
+                          ));
+                    },
+                    child: const Text('Start Game'),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
-      home: const MyHomePage(title: 'Icelandic App Stuff'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
+class QuizPage extends StatelessWidget {
   String enteredWord1 = "";
   String enteredWord2 = "";
   String enteredWord3 = "";
   bool subjectBool = false;
   bool verbBool = false;
   bool objectBool = false;
+  int turnCounter;
+
+  QuizPage(this.turnCounter);
 
   int getRandomListItem(list) {
     Random random = Random();
@@ -103,22 +123,9 @@ class _MyHomePageState extends State<MyHomePage> {
     }
   }
 
-  void checkAnswer() {
-    if (subjectBool && verbBool && objectBool) {
-      print("All correct!");
-    } else {
-      print("Not quite there...");
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+
     List useWords = getWords();
     String isSubject = useWords[0];
     String enSubject = useWords[1];
@@ -129,13 +136,9 @@ class _MyHomePageState extends State<MyHomePage> {
 
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text("Icelandic app stuff"),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
@@ -235,14 +238,162 @@ class _MyHomePageState extends State<MyHomePage> {
                           textStyle: const TextStyle(fontSize: 15),
                         ),
                         onPressed: () {
-                          checkAnswer();
+                          if (subjectBool && verbBool && objectBool) {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => GotItRightPage(turnCounter, "$enSubject $enVerb $enObject", "$isSubject $isVerb $isObject"),
+                                ));
+                          } else {
+                            showDialog<String>(
+                              context: context,
+                              builder: (BuildContext context) => AlertDialog(
+                                content: const Text('Not quite!'),
+                                actions: <Widget>[
+                                  TextButton(
+                                    onPressed: () => Navigator.pop(context, 'Keep trying'),
+                                    child: const Text('Keep trying'),
+                                  ),
+                                  TextButton(
+                                    onPressed: () {
+                          Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                          builder: (context) => GaveUpPage(turnCounter, "$enSubject $enVerb $enObject", "$isSubject $isVerb $isObject"),
+                          ));
+                          },
+                                    child: const Text('Give up'),
+                                  ),
+                                ],
+                              ),
+                            );
+                          }
                         },
                         child: const Text('Check Answer'),
                       ),
                     ),
                   ),
                 ),
+
               ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class GotItRightPage extends StatelessWidget {
+  int turnCounter;
+  String enSentence;
+  String isSentence;
+
+  GotItRightPage(this.turnCounter, this.enSentence, this.isSentence);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+    body: Center(
+      child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+                "That's right!",
+                textAlign: TextAlign.center,
+              ),
+            Text(
+              enSentence,
+              textAlign: TextAlign.center,
+            ),
+            Text(
+              isSentence,
+              textAlign: TextAlign.center,
+            ),
+            Container(
+              margin: const EdgeInsets.only(top: 50.0),
+              width: MediaQuery.of(context).size.width,
+              child: Center(
+                child: Container(
+                  width: 150.0,
+                  height: 50.0,
+                  color: Colors.amber[600],
+                  child: TextButton(
+                    style: TextButton.styleFrom(
+                      padding: const EdgeInsets.all(16.0),
+                      primary: Colors.red,
+                      textStyle: const TextStyle(fontSize: 15),
+                    ),
+                    onPressed: () {
+                      turnCounter = turnCounter + 1;
+                      print(turnCounter);
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => QuizPage(turnCounter),
+                          ));
+                    },
+                    child: const Text('Start again'),
+                  ),
+                ),
+              ),
+            ),
+            ],
+      ),
+    ),
+    );
+  }
+}
+
+class GaveUpPage extends StatelessWidget {
+  int turnCounter;
+  String enSentence;
+  String isSentence;
+
+  GaveUpPage(this.turnCounter, this.enSentence, this.isSentence);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              enSentence,
+              textAlign: TextAlign.center,
+            ),
+            Text(
+              isSentence,
+              textAlign: TextAlign.center,
+            ),
+            Container(
+              margin: const EdgeInsets.only(top: 50.0),
+              width: MediaQuery.of(context).size.width,
+              child: Center(
+                child: Container(
+                  width: 150.0,
+                  height: 50.0,
+                  color: Colors.amber[600],
+                  child: TextButton(
+                    style: TextButton.styleFrom(
+                      padding: const EdgeInsets.all(16.0),
+                      primary: Colors.red,
+                      textStyle: const TextStyle(fontSize: 15),
+                    ),
+                    onPressed: () {
+                      turnCounter = turnCounter + 1;
+                      print(turnCounter);
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => QuizPage(turnCounter),
+                          ));
+                    },
+                    child: const Text('Start again'),
+                  ),
+                ),
+              ),
             ),
           ],
         ),
